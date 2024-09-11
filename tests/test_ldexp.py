0f250f44cabeed2860042c0984ea461aec378fe7
eyadellai1 <132475110+eyadellai1@users.noreply.github.com>
2024-09-11 21:20:06 +0300

    add screens

--- tests/test_ldexp.py
+++ tests/test_ldexp.py
@@ -1,30 +1,52 @@
 # tests/test_ldexp.py
-from ldexp_function import tracked_ldexp, coverage_tracker
-from coverage_tracker import print_coverage_report
+import math
+import pytest
+
+
+
+
+def wrapper(x,i):
+    if not x or not i:
+        raise ValueError("provide integers, currently x or i is missing ")
+    print(isinstance(x,int))
+    if isinstance(x,int) and isinstance(i,int):
+        return math.ldexp(x,i)
+    else:
+        raise ValueError("provide integers, currently x or i is not integers")
 
 class TestMathLdexp:
     def test_positive_numbers(self):
-        assert tracked_ldexp(1, 3) == 8
+        # Verify that tracked_ldexp behaves like math.ldexp
+        print(wrapper(9,3))
+        assert wrapper(9, 3) == 72
 
     def test_negative_numbers(self):
-        assert tracked_ldexp(-2, 2) == -8
+        # Verify with negative numbers
+        assert wrapper(-5, 2) == math.ldexp(-5, 2)
 
     def test_fractional_x(self):
-        assert tracked_ldexp(0.5, 2) == 2
+        # Verify fractional numbers
+       with pytest.raises(ValueError):
+        assert wrapper(0.5, 2)
 
-    def test_zero_x(self):
-        assert tracked_ldexp(0, 5) == 0
+    def test_string(self):
+        # Verify with x = 0
+       with pytest.raises(ValueError):
+        assert wrapper("eya", 5)
 
-    def test_large_i(self):
-        assert tracked_ldexp(1, 1000) == 2 ** 1000
+    def test_missing(self):
+        # Verify with a large exponent
+       with pytest.raises(TypeError):
+        assert wrapper()== math.ldexp(1, 1000)
 
+# Running the tests
 if __name__ == '__main__':
     test = TestMathLdexp()
     test.test_positive_numbers()
     test.test_negative_numbers()
     test.test_fractional_x()
-    test.test_zero_x()
-    test.test_large_i()
+    test.test_string()
+    test.test_missing()
 
-    print(coverage_tracker)
+
 
